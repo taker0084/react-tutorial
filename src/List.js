49037500689afcd7083eaacd0f8b790397e9adf9
Takeru Seki <0084takeru@gmail.com>
2024-09-03 23:05:07 +0900

    Formから言語の追加を実装

--- src/List.js
+++ src/List.js
@@ -1,19 +1,14 @@
 import React from 'react';
+import { LANGUAGES } from './const/languages';
 
-const LANGUAGES = [      //言語の配列
-    'JavaScript',
-     'C++',
-     'Ruby',
-     'Java', 
-     'PHP', 
-     'Go'
-    ];
+
 export class List extends React.Component {   //親コンポーネントからの引数を受け取る
     render() {
+        const {langs} = this.props;
         return (                         //return内に子コンポーネントの中身を記述
             <div>
                 {
-                    LANGUAGES.map((lang,index) => {                {/*mapメソッドで配列をhtmlに変換,keyは一意の値を指定、必ず明記*/}
+                    langs.map((lang,index) => {                {/*mapメソッドで配列をhtmlに変換,keyは一意の値を指定、必ず明記*/}
                         return <div key={index}>{lang}</div>       
                     })
                 }
---
+++ src/const/languages.js
@@ -0,0 +1,8 @@
+export const LANGUAGES = [      //言語の配列
+    'JavaScript',
+     'C++',
+     'Ruby',
+     'Java', 
+     'PHP', 
+     'Go'
+];
