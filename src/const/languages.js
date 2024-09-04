3132218cb65e94a6bea20d6430a703c18e7844aa
Takeru Seki <0084takeru@gmail.com>
2024-09-04 14:59:21 +0900

    一部修正

--- src/const/languages.js
+++ src/const/languages.js
@@ -1,12 +1,12 @@
 export const LANGUAGES = [      //言語の配列
     'JavaScript',
-     'C++',
-     'Ruby',
-     'Java', 
-     'PHP', 
-     'Go'
+    'C++',
+    'Ruby',
+    'Java', 
+    'PHP', 
+    'Go'
 ];
- export const getLanguages = () => {
+export const getLanguages = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(LANGUAGES);
